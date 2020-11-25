2083e071973aa81f1bf5a2c8bb2980fa58143086
toduko <teddyepich@gmail.com>
2020-11-25 14:34:24 +0200

    Refactored Multiple Choice Questions

--- app/src/main/java/com/toduko/ltc/QuestionMultipleChoice.kt
+++ app/src/main/java/com/toduko/ltc/QuestionMultipleChoice.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
+import android.widget.Toast
 import com.toduko.ltc.databinding.FragmentQuestionMultipleChoiceBinding
 import kotlinx.coroutines.selects.select
 
@@ -25,10 +26,15 @@ class QuestionMultipleChoice : Fragment() {
         binding.choice3.text = question.answer3
         binding.questionTitle.text = question.title
         binding.button3.setOnClickListener {
-
+            var resultText: String
             val checkButton =
                 binding.root.findViewById<RadioButton>(binding.questions.checkedRadioButtonId)
-            Log.i("test", (checkButton.text.toString() == question.correctAnswer).toString())
+            if (binding.questions.checkedRadioButtonId != -1) {
+                val answeredCorrectly = checkButton.text.toString() == question.correctAnswer
+                if (answeredCorrectly) resultText = "Correct Answer"
+                else resultText = "Wrong Answer"
+            } else resultText = "You must first select an answer"
+            Toast.makeText(binding.root.context, resultText, Toast.LENGTH_LONG).show()
         }
         return binding.root
     }
