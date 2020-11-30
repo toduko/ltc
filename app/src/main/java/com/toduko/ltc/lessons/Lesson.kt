84c80d3e17d86ddf202fdbfaa04fb0a24ad9709d
toduko <teddyepich@gmail.com>
2020-11-30 12:46:11 +0200

    Cleaned Up Unused Imports And Files

--- app/src/main/java/com/toduko/ltc/lessons/Lesson.kt
+++ app/src/main/java/com/toduko/ltc/lessons/Lesson.kt
@@ -1,7 +1,6 @@
 package com.toduko.ltc.lessons
 
 import android.os.Bundle
-import android.text.Html
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
--- app/src/main/java/com/toduko/ltc/menus/DifficultySelect.kt
+++ app/src/main/java/com/toduko/ltc/menus/DifficultySelect.kt
@@ -1,26 +1,14 @@
 package com.toduko.ltc.menus
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.navigation.findNavController
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import com.toduko.ltc.R
 import com.toduko.ltc.databinding.FragmentDifficultySelectBinding
-
 
 class DifficultySelect : Fragment() {
     override fun onCreateView(
