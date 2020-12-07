16485e341e3d3df7f76bf8b615a7b8df19824363
Ivan Ivanov <Ivanivanov.ii726@gmail.com>
2020-12-07 12:05:32 +0200

    Lessons list design changed. Dark mode disabled.

--- app/src/main/java/com/toduko/ltc/MainActivity.kt
+++ app/src/main/java/com/toduko/ltc/MainActivity.kt
@@ -2,10 +2,12 @@ package com.toduko.ltc
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_main)
     }
 }
